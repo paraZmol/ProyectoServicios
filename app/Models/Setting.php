27d7af86f3732df06d4ff1aeb06f0f2d74f25c6a
paraZmol <nunes.20.24@gmail.com>
2025-11-26 00:22:31 -0500

    feat: Configurar el modelo de ajustes y servicios

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre_empresa',
+        'telefono',
+        'email',
+        'simbolo_moneda',
+        'iva_porcentaje',
+        'direccion',
+        'ciudad',
+    ];
+
+    public $timestamps = true;
 }
