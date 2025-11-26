6e93f430fdd26326b41e0a06a1bb39f2f4577d05
paraZmol <nunes.20.24@gmail.com>
2025-11-26 00:10:42 -0500

    feat: Agregar el modelo cliente

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'telefono',
+        'email',
+        'direccion',
+        'estado',
+    ];
+
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
